7711acc96c4c0b2fd329af2861b59a440a8c829a
GRRINO <yy79250@gmail.com>
2024-02-05 18:24:13 +0630

    Add project files.

---
+++ DotNetTrainingBatch3.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+//ctrl + k , c => disable
+//ctrl + k + u => enable
+
+#region Read
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = "DESKTOP-G92HAN0\\SQLEXPRESS01";
+sqlConnectionStringBuilder.InitialCatalog = "TestDb";
+sqlConnectionStringBuilder.UserID = "sa";
+sqlConnectionStringBuilder.Password = "yy@79250";
+
+string query = "select * from tbl_blog";
+SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+sqlConnection.Open();
+
+SqlCommand cmd = new SqlCommand(query,sqlConnection);
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+DataTable dt =new DataTable();
+adapter.Fill(dt);
+sqlConnection.Close();
+
+//dataset
+//datatable
+//datarow
+//datacolumn
+
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine(dr["BlogId"]);
+    Console.WriteLine(dr["BlogTitle"]);
+    Console.WriteLine(dr["BlogAuthor"]);
+    Console.WriteLine(dr["BlogContent"]);
+}
+
+#endregion
+
+Console.ReadLine();
